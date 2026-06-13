5f563849e532f16e19b73afb6a803eea7c8c5253
George <gcollier229@gmail.com>
2026-06-13 17:30:29 +1200

    Keep only part 2

--- testing.js
+++
@@ -1,6 +0,0 @@
-let date = "2026-03-32";
-let fDate = date.split("-");
-console.log(fDate); //  ["2026", "03", "32"]
-let fDate2  = fDate.reverse().join('-'); 
-console.log(fDate2); // "32-03-2026"
-
